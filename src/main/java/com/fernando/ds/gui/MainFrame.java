d490e4f38728ae0761dfd858eca26f1a994da6d9
Fernando Antonio Fonteles Oliveira <112741054+FernandoAFOliveira@users.noreply.github.com>
2026-07-23 16:55:40 -0400

    Add resilient feature menus to desktop app

--- src/main/java/com/fernando/ds/gui/MainFrame.java
+++ src/main/java/com/fernando/ds/gui/MainFrame.java
@@ -1,18 +1,16 @@
 package com.fernando.ds.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
-
 import javax.swing.JScrollPane;
-
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Toolkit;
-
 
 import com.fernando.ds.util.ContentLoader;
 
@@ -45,69 +43,137 @@ public class MainFrame extends JFrame {
         );
 
         AppController controller = new AppController(
-                questionPanel,
-                dsListPanel,
-                diagramPanel,
-                explanationPanel
-            );
+            questionPanel,
+            dsListPanel,
+            diagramPanel,
+            explanationPanel
+        );
 
-        setJMenuBar(createMenuBar(controller, explanationPanel));
-
-        controller.reset();
-
+        setJMenuBar(createMenuBar(controller));
+        UiActionGuard.run(this, "Advisor", controller::reset);
         add(mainPanel, BorderLayout.CENTER);
-
     }
-    
-    private JMenuBar createMenuBar(AppController controller, ExplanationPanel explanationPanel){
-        JMenuBar menuBar = new JMenuBar();
 
+    private JMenuBar createMenuBar(AppController controller) {
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.add(createFileMenu(controller));
+        menuBar.add(createSubjectMenu());
+        menuBar.add(createExperienceMenu());
+        menuBar.add(createViewMenu(controller));
+        menuBar.add(createHelpMenu());
+        return menuBar;
+    }
+
+    private JMenu createFileMenu(AppController controller) {
         JMenu fileMenu = new JMenu("File");
         JMenuItem resetItem = new JMenuItem("Reset selections");
         JMenuItem exitItem = new JMenuItem("Exit");
 
-        resetItem.addActionListener(e -> controller.reset());
-        exitItem.addActionListener(e -> dispose());
+        resetItem.addActionListener(event ->
+            UiActionGuard.run(this, "Reset selections", controller::reset)
+        );
+        exitItem.addActionListener(event -> dispose());
 
         fileMenu.add(resetItem);
         fileMenu.addSeparator();
         fileMenu.add(exitItem);
+        return fileMenu;
+    }
 
+    private JMenu createSubjectMenu() {
+        JMenu subjectMenu = new JMenu("Subject");
+        JMenuItem javaItem = new JMenuItem("Java (active)");
+        JMenuItem cItem = new JMenuItem("C");
+        JMenuItem cppItem = new JMenuItem("C++");
+        JMenuItem pythonItem = new JMenuItem("Python");
+
+        javaItem.addActionListener(event -> JOptionPane.showMessageDialog(
+            this,
+            "Java is the active subject.",
+            "Java",
+            JOptionPane.INFORMATION_MESSAGE
+        ));
+        addNotEnabledAction(cItem, "C");
+        addNotEnabledAction(cppItem, "C++");
+        addNotEnabledAction(pythonItem, "Python");
+
+        subjectMenu.add(javaItem);
+        subjectMenu.addSeparator();
+        subjectMenu.add(cItem);
+        subjectMenu.add(cppItem);
+        subjectMenu.add(pythonItem);
+        return subjectMenu;
+    }
+
+    private JMenu createExperienceMenu() {
+        JMenu experienceMenu = new JMenu("Experience");
+        JMenuItem advisorItem = new JMenuItem("Advisor (active)");
+        JMenuItem explorerItem = new JMenuItem("Explorer");
+        JMenu learnMenu = new JMenu("Learn");
+        JMenuItem flashCardsItem = new JMenuItem("Flash Cards");
+        JMenuItem timedQuizItem = new JMenuItem("Timed Quiz");
+
+        advisorItem.addActionListener(event -> JOptionPane.showMessageDialog(
+            this,
+            "Advisor is the active experience.",
+            "Advisor",
+            JOptionPane.INFORMATION_MESSAGE
+        ));
+        addNotEnabledAction(explorerItem, "Explorer");
+        addNotEnabledAction(flashCardsItem, "Flash Cards");
+        addNotEnabledAction(timedQuizItem, "Timed Quiz");
+
+        learnMenu.add(flashCardsItem);
+        learnMenu.add(timedQuizItem);
+        experienceMenu.add(advisorItem);
+        experienceMenu.add(explorerItem);
+        experienceMenu.add(learnMenu);
+        return experienceMenu;
+    }
+
+    private JMenu createViewMenu(AppController controller) {
         JMenu viewMenu = new JMenu("View");
         JMenu themeMenu = new JMenu("Theme");
 
-        JMenuItem lightThemeItem = new JMenuItem("Light");
-        JMenuItem softBlueThemeItem = new JMenuItem("Soft Blue");
-        JMenuItem darkThemeItem = new JMenuItem("Dark");
-        JMenuItem darkBlueThemeItem = new JMenuItem("Dark Blue");
-
-        themeMenu.add(lightThemeItem);
-        themeMenu.add(softBlueThemeItem);
-        themeMenu.add(darkThemeItem);
-        themeMenu.add(darkBlueThemeItem);
+        addThemeItem(themeMenu, "Light", Theme.LIGHT, controller);
+        addThemeItem(themeMenu, "Soft Blue", Theme.SOFT_BLUE, controller);
+        addThemeItem(themeMenu, "Dark", Theme.DARK, controller);
+        addThemeItem(themeMenu, "Dark Blue", Theme.DARK_BLUE, controller);
 
         viewMenu.add(themeMenu);
+        return viewMenu;
+    }
 
+    private JMenu createHelpMenu() {
         JMenu helpMenu = new JMenu("Help");
         JMenuItem aboutItem = new JMenuItem("About");
 
-        aboutItem.addActionListener(e -> showAboutDialog());
-
+        aboutItem.addActionListener(event ->
+            UiActionGuard.run(this, "About", this::showAboutDialog)
+        );
         helpMenu.add(aboutItem);
+        return helpMenu;
+    }
 
-        menuBar.add(fileMenu);
-        menuBar.add(viewMenu);
-        menuBar.add(helpMenu);
+    private void addThemeItem(
+        JMenu menu,
+        String label,
+        Theme theme,
+        AppController controller
+    ) {
+        JMenuItem item = new JMenuItem(label);
+        item.addActionListener(event -> UiActionGuard.run(
+            this,
+            label + " theme",
+            () -> applyTheme(theme, controller)
+        ));
+        menu.add(item);
+    }
 
-        lightThemeItem.addActionListener(e -> applyTheme(Theme.LIGHT, controller));
-
-        softBlueThemeItem.addActionListener(e -> applyTheme(Theme.SOFT_BLUE, controller));
-
-        darkThemeItem.addActionListener(e -> applyTheme(Theme.DARK, controller));
-
-        darkBlueThemeItem.addActionListener(e -> applyTheme(Theme.DARK_BLUE, controller));
-
-        return menuBar;
+    private void addNotEnabledAction(JMenuItem item, String featureName) {
+        item.addActionListener(event ->
+            UiActionGuard.showNotEnabled(this, featureName)
+        );
     }
 
     private void applyTheme(Theme theme, AppController controller) {
@@ -136,5 +202,4 @@ public class MainFrame extends JFrame {
             JOptionPane.INFORMATION_MESSAGE
         );
     }
-    
 }
